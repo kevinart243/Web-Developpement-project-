78742f42afc11c0a8761300364b26d094bc9fdd5
aiden-carroll <aidencarroll00@gmail.com>
2024-12-07 17:13:22 -0700

    Additional work on the cart

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,10 @@ function add_to_cart(name, price) {
     cart.append({"name": name, "price": price})
 }
 
+function load_cart() {
+    
+}
+
 counter = document.getElementById("cart-count")
 if (counter != null) {
     counter.innerText = cart.length
